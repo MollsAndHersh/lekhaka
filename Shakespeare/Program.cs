e13c2faee80b015f2bbd563219f47502fed823ef
bhagi <bhargavak37@gmail.com>
2017-07-22 04:12:11 +0530

    Add project files.

---
+++ Shakespeare/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shakespeare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Creature;
+            string Luminous;
+            string Ghastly;
+            string Spectral;
+            string Countryman;
+            string Farrier;
+            string Farmer;
+            string Dreadful;
+            string Apparition;
+            string Hound;
+            string Story;
+            Console.WriteLine("-------------------------------------");
+            Console.WriteLine("            Shakespeare!             ");
+            Console.WriteLine("-------------------------------------");
+            //ask player to enter words
+            Console.Write("Please enter a noun: ");
+            Creature = Console.ReadLine();
+            Console.Write("Please enter an adjective: ");
+            Ghastly = Console.ReadLine();
+            Console.Write("Please enter an occupation: ");
+            Countryman = Console.ReadLine();
+            Console.Write("Please enter an occupation: ");
+            Farrier = Console.ReadLine();
+            Console.Write("Please enter an occupation: ");
+            Farmer = Console.ReadLine();
+            Console.Write("PLease enter an adjective");
+            Dreadful = Console.ReadLine();
+            Console.Write("Please enter a noun: ");
+            Apparition = Console.ReadLine();
+            Console.Write("Please enter a noun: ");
+            Hound = Console.ReadLine();
+
+        }
+    }
+}
